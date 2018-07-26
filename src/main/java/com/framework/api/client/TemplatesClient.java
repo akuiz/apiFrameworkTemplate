a9ea4f50fddbb5b456013e520c42e11968c96936
akuiz <arkuziaev@gmail.com>
2018-07-26 17:43:26 +0400

    Added basic operations into TemplatesClient

---
+++ src/main/java/com/framework/api/client/TemplatesClient.java
@@ -0,0 +1,22 @@
+package com.framework.api.client;
+
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+import feign.Response;
+
+public interface TemplatesClient {
+
+    @RequestLine("[POST] /api/templates")
+    @Headers("Content-Type: application/json")
+    Response createTemplate();
+
+    @RequestLine("[DELETE] /api/templates/{templateID}")
+    @Headers("Content-Type: application/json")
+    Response deleteTemplate(@Param("templateID") String templateID);
+
+    @RequestLine("[PUT] /api/templates}")
+    @Headers("Content-Type: application/json")
+    Response updateTemplate();
+
+}
