5f2da2c4480c1a049ff6d611e91b37fd9fc76e45
akuiz <arkuziaev@gmail.com>
2018-07-29 02:05:55 +0400

    API Configuration file added using Spring framework

--- src/main/java/com/framework/configuration/TestConfigurationAPI.java
+++ src/main/java/com/framework/configuration/TestConfigurationAPI.java
@@ -1,4 +1,16 @@
 package com.framework.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+@ComponentScan(value = "com.framework.api.step")
 public class TestConfigurationAPI {
+
+    @Value("${dms.url}")
+    String dmsURL;
+
 }
