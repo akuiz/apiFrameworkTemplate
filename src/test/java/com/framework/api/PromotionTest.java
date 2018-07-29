600dbf253ec39dfa5831c78ee0e33dc2ba3fd5a6
akuiz <arkuziaev@gmail.com>
2018-07-29 20:44:41 +0400

    Added basic add promotion test

---
+++ src/test/java/com/framework/api/PromotionTest.java
@@ -0,0 +1,26 @@
+package com.framework.api;
+
+import com.framework.api.step.PromotionStep;
+import com.framework.configuration.TestConfigurationAPI;
+import com.framework.model.Id;
+import com.framework.model.Promotion;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@Feature("API tests")
+@Story("Promotion test")
+@ContextConfiguration(classes = TestConfigurationAPI.class)
+public class PromotionTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private PromotionStep promotionStep;
+
+    @Test
+    public void addPromotionTest(){
+        Id createdPromotionID = promotionStep.addPromotion(Promotion.testPromotion());
+    }
+}
