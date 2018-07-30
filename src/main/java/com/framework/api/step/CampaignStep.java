8cc4a387389f9515a3faa6d2daff2c480054b0ee
akuiz <arkuziaev@gmail.com>
2018-07-30 23:49:02 +0400

    Add 'Add campaign' step

--- src/main/java/com/framework/api/step/CampaignStep.java
+++ src/main/java/com/framework/api/step/CampaignStep.java
@@ -1,4 +1,19 @@
 package com.framework.api.step;
 
+import com.framework.api.client.CampaignsClient;
+import com.framework.model.campaign.Campaign;
+import com.framework.model.campaign.CreateCampaignResponse;
+import io.qameta.allure.Step;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CampaignStep {
+
+    @Autowired
+    CampaignsClient campaignsClient;
+
+    @Step("Add campaign")
+    public CreateCampaignResponse addCampaign(Campaign campaign){
+        CreateCampaignResponse response = campaignsClient.addCampaign(campaign);
+        return response;
+    }
 }
