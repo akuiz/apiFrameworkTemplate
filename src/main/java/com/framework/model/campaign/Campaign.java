7ef4a732549dd1de9e678d28c8cdf6faac519b21
akuiz <arkuziaev@gmail.com>
2018-07-30 13:10:13 +0400

    Added discounts field into Campaign model

--- src/main/java/com/framework/model/campaign/Campaign.java
+++ src/main/java/com/framework/model/campaign/Campaign.java
@@ -1,10 +1,13 @@
 package com.framework.model.campaign;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.framework.model.promotion.TemplateID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder(toBuilder = true)
@@ -18,4 +21,5 @@ public class Campaign {
     private String start;
     private String end;
     private String id;
+    private List<TemplateID> discounts;
 }
