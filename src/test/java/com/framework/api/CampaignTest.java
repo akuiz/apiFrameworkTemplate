cf84056183d661a772f718455a65f402ed723c46
akuiz <arkuziaev@gmail.com>
2018-07-31 15:19:06 +0400

    Added simple campaign creation test into CampaignTest

--- src/test/java/com/framework/api/CampaignTest.java
+++ src/test/java/com/framework/api/CampaignTest.java
@@ -3,12 +3,19 @@ package com.framework.api;
 import com.framework.api.step.CampaignStep;
 import com.framework.api.step.PromotionStep;
 import com.framework.configuration.TestConfigurationAPI;
+import com.framework.model.Id;
+import com.framework.model.campaign.Campaign;
+import com.framework.model.campaign.CreateCampaignResponse;
+import com.framework.model.promotion.Promotion;
+import com.framework.model.promotion.TemplateID;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
 
 @Feature("API tests")
 @Story("Campaign test")
@@ -21,5 +28,11 @@ public class CampaignTest extends AbstractTestNGSpringContextTests{
     private CampaignStep campaignStep;
 
     @Test(description = "Add campaign test")
-    public void addCampaign(){}
+    public void addCampaign(){
+        Id promotionID = promotionStep.addPromotion(Promotion.testPromotion());
+        TemplateID promotionTemplateID = new TemplateID(promotionID);
+        Campaign campaign = Campaign.campaignWithPromotionIDS("testCampaignAPI",
+                Arrays.asList(promotionTemplateID));
+        CreateCampaignResponse createCampaignResponse = campaignStep.addCampaign(campaign);
+    }
 }
