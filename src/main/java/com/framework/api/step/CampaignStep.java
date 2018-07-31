5165f8ac97ae278c1f215819d454e1968f407c5e
akuiz <arkuziaev@gmail.com>
2018-07-31 15:13:11 +0400

    Added component annotation to CampaignStep

--- src/main/java/com/framework/api/step/CampaignStep.java
+++ src/main/java/com/framework/api/step/CampaignStep.java
@@ -5,7 +5,9 @@ import com.framework.model.campaign.Campaign;
 import com.framework.model.campaign.CreateCampaignResponse;
 import io.qameta.allure.Step;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CampaignStep {
 
     @Autowired
