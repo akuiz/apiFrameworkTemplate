f9cf769003c6ff85e41cfa7e31eba41651c267eb
akuiz <arkuziaev@gmail.com>
2018-08-02 14:43:31 +0400

    Make ID field in CampaignModel string

--- src/main/java/com/framework/model/campaign/Campaign.java
+++ src/main/java/com/framework/model/campaign/Campaign.java
@@ -1,6 +1,7 @@
 package com.framework.model.campaign;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.framework.model.Id;
 import com.framework.model.promotion.TemplateID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,6 +9,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+
+import static com.framework.api.utils.CodeUtils.convertList;
 
 @Data
 @Builder(toBuilder = true)
@@ -29,6 +32,16 @@ public class Campaign {
                 .start("2018-07-01")
                 .end("2018-07-31")
                 .discounts(templateIDs)
+                .build();
+    }
+
+    public EditedCampaign prepareCampaignToEditRequest(CreateCampaignResponse response){
+        return new EditedCampaign().toBuilder()
+                .name(name)
+                .id(response.getId().getId())
+                .discounts(convertList(response.getDiscountIDs(), discountID -> new Id(discountID)))
+                .start(start)
+                .end(end)
                 .build();
     }
 }
