707509ad322e51d710ffe6d90d38385c34178f85
akuiz <arkuziaev@gmail.com>
2018-08-04 22:30:28 +0400

    ForecastClient now accepts Forecast request model object

--- src/main/java/com/framework/api/client/ForecastClient.java
+++ src/main/java/com/framework/api/client/ForecastClient.java
@@ -1,5 +1,6 @@
 package com.framework.api.client;
 
+import com.framework.api.model.forecast.ForecastRequest;
 import feign.Headers;
 import feign.RequestLine;
 import feign.Response;
@@ -7,5 +8,5 @@ import feign.Response;
 public interface ForecastClient {
     @RequestLine("POST /api/forecast/false/0")
     @Headers("Content-Type: application/json")
-    Response buildForecast();
+    Response buildForecast(ForecastRequest forecastRequest);
 }
