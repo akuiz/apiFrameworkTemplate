b2caf6bfd7e3976f2351f0f527171d1c9278dba9
akuiz <arkuziaev@gmail.com>
2018-08-05 11:02:21 +0400

    UI tests configuration file modified to access application.properties file

--- src/main/java/com/framework/configuration/TestConfigurationUI.java
+++ src/main/java/com/framework/configuration/TestConfigurationUI.java
@@ -1,4 +1,25 @@
 package com.framework.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@PropertySource("application.properties")
 public class TestConfigurationUI {
+
+    @Value("${browser}")
+    public String browser;
+
+    @Value("${dev.url}")
+    public String url;
+
+    @PostConstruct
+    public void setProperties() {
+        com.codeborne.selenide.Configuration.browser = browser;
+        com.codeborne.selenide.Configuration.baseUrl = url;
+    }
+
 }
