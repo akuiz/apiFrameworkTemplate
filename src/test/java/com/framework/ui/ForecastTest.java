3752114299129beee217e2875ccb72f704543dfe
akuiz <arkuziaev@gmail.com>
2018-08-05 11:20:01 +0400

    Added Test files for all functionalities

---
+++ src/test/java/com/framework/ui/ForecastTest.java
@@ -0,0 +1,9 @@
+package com.framework.ui;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Feature("UI tests")
+@Story("Forecast test")
+public class ForecastTest {
+}
---
+++ src/test/java/com/framework/ui/PromotionTest.java
@@ -0,0 +1,9 @@
+package com.framework.ui;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+
+@Feature("UI tests")
+@Story("Promotion test")
+public class PromotionTest {
+}
