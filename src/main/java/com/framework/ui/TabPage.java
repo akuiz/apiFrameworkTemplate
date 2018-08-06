4dd2427c2fc887e6436fdc8888e6458a9ac6a47d
akuiz <arkuziaev@gmail.com>
2018-08-06 15:07:45 +0400

    TabPage class for all tabs created

---
+++ src/main/java/com/framework/ui/TabPage.java
@@ -0,0 +1,22 @@
+package com.framework.ui;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.not;
+import static com.codeborne.selenide.Condition.visible;
+
+public class TabPage extends PageObject{
+
+    private SelenideElement loader = root.$(".mat-progress-spinner");
+    private long downloadTimeout = 5000;
+
+    public TabPage(SelenideElement root) {
+        super(root);
+        waitForDownload();
+    }
+
+    protected void waitForDownload(){
+        loader.waitUntil(not(visible), downloadTimeout);
+    }
+
+}
