3a975b339a4513fa6db77b664725d84d00bebc92
akuiz <arkuziaev@gmail.com>
2018-08-06 15:10:06 +0400

    Abstract test to open main url before each tests added

--- src/test/java/com/framework/ui/AbstractTest.java
+++ src/test/java/com/framework/ui/AbstractTest.java
@@ -3,7 +3,16 @@ package com.framework.ui;
 import com.framework.configuration.TestConfigurationUI;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Selenide.open;
 
 @ContextConfiguration(classes = TestConfigurationUI.class)
-public class AbstractTest extends AbstractTestNGSpringContextTests{
+public abstract class AbstractTest extends AbstractTestNGSpringContextTests{
+
+    @BeforeMethod
+    public void openPage(){
+        open("");
+    }
+
 }
