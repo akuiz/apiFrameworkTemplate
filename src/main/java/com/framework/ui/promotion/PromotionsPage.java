7540016b1c6ec1183e9026c4819b133b9bc1df6f
akuiz <arkuziaev@gmail.com>
2018-08-06 16:59:05 +0400

    ClickAddPromotionButton method added into PromotionsPage

--- src/main/java/com/framework/ui/promotion/PromotionsPage.java
+++ src/main/java/com/framework/ui/promotion/PromotionsPage.java
@@ -1,14 +1,21 @@
 package com.framework.ui.promotion;
 
+import com.codeborne.selenide.SelenideElement;
 import com.framework.ui.TabPage;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class PromotionsPage extends TabPage{
 
+    private SelenideElement addPromotionButton = $(".btn-add-promo");
+
     public PromotionsPage() {
         super($("app-promotions-page"));
     }
 
+    public CreatePromotion clickAddPromotionButton(){
+        addPromotionButton.click();
+        return new CreatePromotion();
+    }
 
 }
