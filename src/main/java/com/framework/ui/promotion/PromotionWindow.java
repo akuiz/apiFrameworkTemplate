061935656d5836e9247de99893b56d552b851035
akuiz <arkuziaev@gmail.com>
2018-08-06 17:04:30 +0400

    PromotionWindow Class has been updated with elements

--- src/main/java/com/framework/ui/promotion/PromotionWindow.java
+++ src/main/java/com/framework/ui/promotion/PromotionWindow.java
@@ -1,13 +1,31 @@
 package com.framework.ui.promotion;
 
+import com.codeborne.selenide.SelenideElement;
 import com.framework.ui.PageObject;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public abstract class PromotionWindow extends PageObject {
+public abstract class PromotionWindow<T> extends PageObject {
+
+    protected SelenideElement closeWindowButton = $(".close-dialog");
+    protected SelenideElement name = $(".name__input");
+    protected SelenideElement brands = $(".rule__input");
+    protected SelenideElement type = $(".type__input");
+    protected SelenideElement value = $(".value__input");
+    protected SelenideElement confirmAction = $(".btn__title");
 
     public PromotionWindow() {
         super($("app-dialog-add-edit-promo"));
+    }
+
+    protected PromotionsPage close(){
+        closeWindowButton.click();
+        return new PromotionsPage();
+    }
+
+    public T setName(String promotionName){
+        name.setValue(promotionName);
+        return (T)this;
     }
 
 }
