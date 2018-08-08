b9e87847d208270a3f61661ac2feb8095fe49d84
akuiz <arkuziaev@gmail.com>
2018-08-08 12:03:28 +0400

    Added basic promotion test

--- src/test/java/com/framework/ui/PromotionTest.java
+++ src/test/java/com/framework/ui/PromotionTest.java
@@ -1,9 +1,20 @@
 package com.framework.ui;
 
+import com.framework.ui.promotion.CreatePromotion;
+import com.framework.ui.promotion.PromotionsPage;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.testng.annotations.Test;
 
 @Feature("UI tests")
 @Story("Promotion test")
 public class PromotionTest extends AbstractTest{
+
+    @Test(description = "Basic test", groups = {"ui"})
+    public void basicPromotionTest(){
+        PromotionsPage p = new PromotionsPage();
+        CreatePromotion s = p.clickAddPromotionButton();
+        s.setName("sd");
+    }
+
 }
